68fb60dfad0e80fed953aa82dc43240b8916dd6f
George <georgehargreaves5@outlook.com>
2023-09-24 14:26:21 +0100

    Initial Commit

---
+++ OnBoardingView/OnBoardingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingViewApp.swift
+//  OnBoardingView
+//
+//  Created by George Hargreaves on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
