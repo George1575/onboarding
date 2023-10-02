a4eb5a7d3d8c35781669d901580418af14203fbd
George <georgehargreaves5@outlook.com>
2023-10-02 08:28:07 +0100

    add tab functionality

---
+++ TabView/MainTabView.swift
@@ -0,0 +1,25 @@
+//
+//  TabView.swift
+//  OnBoardingView
+//
+//  Created by George Hargreaves on 02/10/2023.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    
+    @AppStorage("signed_in") var currentUserSignedIn: Bool = false
+
+    var body: some View {
+        
+        
+
+        Text("")
+
+    }
+}
+
+#Preview {
+    TabView()
+}
