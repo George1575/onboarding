8adfba8bd956fb305a0d25a8d377dd9e40ddda5d
George <georgehargreaves5@outlook.com>
2023-10-02 07:41:53 +0100

    Add info to JSON

---
+++ TabView/TabView.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  OnBoardingView
+//
+//  Created by George Hargreaves on 01/10/2023.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
